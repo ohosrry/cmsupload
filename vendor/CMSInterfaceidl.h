63f0f1619ac0bc357b297283a559e11778085f00
dietoad <dietoad@gmail.com>
2012-03-22 10:19:09 +0000

    添加预览功能

--- vendor/CMSInterfaceidl.h
+++ vendor/CMSInterfaceidl.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0500 */
-/* at Mon Mar 19 08:56:40 2012
+/* at Thu Mar 22 11:52:29 2012
  */
 /* Compiler settings for .\CMSInterface.idl:
     Oicf, W1, Zp8, env=Win32 (32b run)
